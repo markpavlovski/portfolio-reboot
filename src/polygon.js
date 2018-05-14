aca32f483327388b53e495e36c4b1b1145ccdfba
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-14 11:48:32 -0700

    refactored code to make it more readable;

--- src/polygon.js
+++ src/polygon.js
@@ -1,24 +1,17 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-  width = 600 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
-
-var svg = d3.select("main").append("svg")
+const svg = d3.select("main")
+  .append("svg")
   .attr("width", innerWidth)
   .attr("height",innerHeight)
-.append("g")
-  // .attr("transform",
-  //       "translate(" + margin.left + "," + margin.top + ")");
 
 
-var x = d3.scaleLinear().range([0, innerWidth]);
-var y = d3.scaleLinear().range([0, innerHeight]);
+const x = d3.scaleLinear().range([0, innerWidth]);
+const y = d3.scaleLinear().range([0, innerHeight]);
 
 x.domain([0, innerWidth]);
 y.domain([0, innerHeight]);
 
-var point = {"x": 240, "y": 310}
 
-var poly = [
+const points = [
       {"x":0, "y":0},
       {"x":0,"y":100},
       {"x":innerWidth,"y":100},
@@ -27,12 +20,13 @@ var poly = [
     ];
 
 
+const stagePoints = points => points
+  .map(point => [x(point.x),y(point.y)].join(','))
+  .join(' ')
+
+
 svg.selectAll("polygon")
-  .data([poly])
+  .data([points])
   .enter()
   .append("polygon")
-  .attr("points",function(d) {
-      return d.map(function(d) {
-          return [x(d.x),y(d.y)].join(",");
-      }).join(" ");
-  });
+  .attr("points", stagePoints)
